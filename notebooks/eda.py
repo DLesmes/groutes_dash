cdaf0491e6fdc2004271273ddaf7c149e834c32a
Usuario <ing.dlesmes@gmail.com>
2025-06-24 19:55:57 -0500

    adding the full data labeling bussines day as bool

--- notebooks/eda.py
+++ notebooks/eda.py
@@ -1,6 +1,7 @@
 """ EDA for the visits data """
 # %%
 import pandas as pd
+from datetime import date
 
 # %% [markdown]
 # ## Load data
@@ -12,6 +13,7 @@ df_business_days = pd.read_csv('../backend/data/resultados_uriel_mazabuel.csv')
 df_visits.info()
 # %%
 df_visits['timestamp'] = pd.to_datetime(df_visits['timestamp']).dt.tz_localize(None)
+df_visits['date'] = pd.to_datetime(df_visits['timestamp']).dt.date
 df_visits[['latitude','longitude']] = df_visits['point'].str.split(',', expand=True).astype(float)
 df_visits.info()
 # %%
@@ -50,36 +52,38 @@ object_features_business_days
 # %%
 df_business_days[object_features_business_days].describe(include='all').T
 # %% [markdown]
-# ## Merge visits with business days
-df_visits_business_days = df_visits.merge(df_business_days, how='right', left_on='timestamp', right_on='inicio')
+# ## labeling business days
+df_visits['business_day'] = False
+df_business_days['business_day'] = True
+df_business_days.index = df_business_days['inicio']
+dict_df_business_days = df_business_days.to_dict(orient='index')
+df_visits['business_day'].map(dict_df_business_days)
 # %%
-df_visits_business_days
-# %% [markdown]
-# ## visits with business days data
-df_visits_business_days.info()
-# %% [markdown]
-# ## droping duplicates on visits with business days data
-df_visits_business_days.drop_duplicates(subset=['timestamp'], inplace=True)
-df_visits_business_days.info()
+df_visits.info()
 # %%
-df_visits_business_days.describe().T
+df_visits.describe().T
 # %%
-df_types_visits_business_days = pd.DataFrame(df_visits_business_days.dtypes)
-object_features_visits_business_days = df_types_visits_business_days[df_types_visits_business_days[0] == 'object'].index.to_list()
-object_features_visits_business_days
+df_types_visits = pd.DataFrame(df_visits.dtypes)
+object_features_visits = df_types_visits[df_types_visits[0] == 'object'].index.to_list()
+object_features_visits
 # %%
-float_features_visits_business_days = df_types_visits_business_days[df_types_visits_business_days[0] == 'float64'].index.to_list()
-float_features_visits_business_days
+float_features_visits = df_types_visits[df_types_visits[0] == 'float64'].index.to_list()
+float_features_visits
 # %%
-date_features_visits_business_days = df_types_visits_business_days[df_types_visits_business_days[0] == 'datetime64[ns]'].index.to_list()
-date_features_visits_business_days
+date_features_visits = df_types_visits[df_types_visits[0] == 'datetime64[ns]'].index.to_list()
+date_features_visits
 # %%
-df_visits_business_days[object_features_visits_business_days].describe(include='all').T
+bool_features_visits = df_types_visits[df_types_visits[0] == 'bool'].index.to_list()
+bool_features_visits
 # %%
-df_visits_business_days.fecha.value_counts()
+df_visits[object_features_visits].describe(include='all').T
 # %%
-df_visits_business_days[df_visits_business_days['fecha'] == '2022-02-12']
+df_visits[bool_features_visits].describe(include='all').T
+# %%
+df_visits[df_visits['date'] == date(2023, 12, 24)]
+# %%
+df_visits[df_visits['date'] == date(2023, 12, 24)].place.value_counts()
 # %% [markdown]
 # ## Save data
-df_visits_business_days.to_csv('../backend/data/visitas_business_days.csv', index=False)
+df_visits.to_csv('../backend/data/visitas_business_days.csv', index=False)
 # %%
